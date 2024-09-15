0c79ccfa50a6bcceff82f7ad49b2bd17e66d3c05
arnavgautam <arnavjgautam@gmail.com>
2024-09-15 15:56:08 -0400

    running sampling for ohio, preparing to run model

--- resstock-3.2.0/run_resstock_model.py
+++ resstock-3.2.0/run_resstock_model.py
@@ -6,15 +6,15 @@ import numpy as np
 
 building_unit_type_to_resstock_category = {"2": 'Multi-Family with 2 - 4 Units', "3_4": 'Multi-Family with 2 - 4 Units', "5_9": 'Multi-Family with 5+ Units', "10_19": 'Multi-Family with 5+ Units', "20_49": 'Multi-Family with 5+ Units', "50_plus": 'Multi-Family with 5+ Units', "SFA": 'Single-Family Attached', "SFD": 'Single-Family Detached' }
 
-parallel_resstock_processes = [subprocess.Popen(f"openstudio {join('workflow','run_analysis.rb')} -y {join('project_columbus','columbus_baseline.yml')} -t {unit_type} -n 1 -k", capture_output=True, text=True) for (unit_type, resstock_category) in building_unit_type_to_resstock_category.items()]
+parallel_resstock_processes = [subprocess.Popen([f"openstudio", f"{join('workflow','run_analysis.rb')}", f"-y", f"{join('project_columbus','columbus_upgrades.yml')}", f"-t", f"{unit_type}", f"-n", f"1", f"-k"]) for (unit_type, resstock_category) in building_unit_type_to_resstock_category.items()]
 
 for p in parallel_resstock_processes:
     p.wait()
 
-for p in parallel_resstock_processes:
-    print("Output:\n", p.stdout)
-    print("Error:\n", p.stderr)
-    print("\n")
+# for p in parallel_resstock_processes:
+#     print("Output:\n", p.stdout)
+#     print("Error:\n", p.stderr)
+#     print("\n")
 
 
 # for (unit_type, resstock_category) in building_unit_type_to_resstock_category.items():
--- resstock-3.2.0/run_resstock_sampling.py
+++ resstock-3.2.0/run_resstock_sampling.py
@@ -7,15 +7,16 @@ from shutil import copyfile
 
 building_unit_type_to_resstock_category = {"2": 'Multi-Family with 2 - 4 Units', "3_4": 'Multi-Family with 2 - 4 Units', "5_9": 'Multi-Family with 5+ Units', "10_19": 'Multi-Family with 5+ Units', "20_49": 'Multi-Family with 5+ Units', "50_plus": 'Multi-Family with 5+ Units', "SFA": 'Single-Family Attached', "SFD": 'Single-Family Detached' }
 
-parallel_resstock_processes = []
+# parallel_resstock_processes = []
 for (unit_type, resstock_category) in building_unit_type_to_resstock_category.items():
     source_ACS_file = join('project_columbus','housing_characteristics',f'Geometry Building Type ACS {unit_type}.tsv')
     dest_ACS_file = join('project_columbus','housing_characteristics',f'Geometry Building Type ACS.tsv')
     copyfile(source_ACS_file, dest_ACS_file)
-    parallel_resstock_processes.append(subprocess.Popen([f"ruby", f"{join('resources','run_sampling.rb')}", f"-p", f"project_columbus", f"-n", f"1", f"-o", f"{join('columbus', f'single_{unit_type}_unit_buildstock.csv')}"]))
+    subprocess.run(f"ruby {join('resources','run_sampling.rb')} -p project_columbus -n 1 -o {join('columbus', f'single_{unit_type}_unit_buildstock.csv')}")
+    # parallel_resstock_processes.append(subprocess.Popen
 
-for p in parallel_resstock_processes:
-    p.wait()
+# for p in parallel_resstock_processes:
+#     p.wait()
 
 # for p in parallel_resstock_processes:
 #     print("Output:\n", p.stdout)
